6ed701d906ab23e0fc95d6007f451ed1a5513d40
EC2 Default User <ec2-user@ip-172-31-24-245.ec2.internal>
2024-10-03 00:14:28 +0000

    adding app script

--- app-on-ec2/app/webapp.py
+++ app-on-ec2/app/webapp.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # when root url is accessed
 @app.route("/")
 def hello_world():
-	return "<p>Hello, Bujjimi!<br>This is Minmini. I love you! <3</p>"
+	return "<p>Hello, All!<br>This is Karthikraj!<br>This is my Home page. <3</p>"
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=8001)
