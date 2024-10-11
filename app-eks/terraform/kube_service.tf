45fc78989271a7ff3b9ba87762f71b512bb92ab7
karthikrajkkr <34535249+karthikrajkkr@users.noreply.github.com>
2024-10-11 12:37:44 -0400

    Update kube_service.tf

--- app-eks/terraform/kube_service.tf
+++ app-eks/terraform/kube_service.tf
@@ -15,3 +15,21 @@ resource "kubernetes_service_v1" "flask_svc" {
   }
   depends_on = [aws_eks_node_group.webapp_node_1]
 }
+
+
+/*
+#YAML format of Service for better understanding
+#---------------------------------------------------
+apiVersion: v1
+kind: Service
+metadata:
+  name: flask-svc
+spec:
+  selector:
+    app: flask
+  ports:
+    - protocol: TCP
+      port: 8080
+      targetPort: 8001
+  type: ClusterIP
+*/
