425c49ddcb0b8744630499a11b70610ce44f7d4d
karthikrajkkr <34535249+karthikrajkkr@users.noreply.github.com>
2024-10-11 12:41:46 -0400

    Update variables.tf

--- app-eks/terraform/variables.tf
+++ app-eks/terraform/variables.tf
@@ -8,8 +8,8 @@ variable "region" { default = "us-east-1" }
 
 variable "node_instance_types" { default = ["t3.medium"] }
 
-variable "private_subnet_ids" { default = ["subnet-03639446afdabc019", "subnet-0f4ba4b32ec2d3452"] }
-
+#Subnets specified must be in at least two different AZs
+variable "private_subnet_ids" { default = ["subnet-03639446afdabc019", "subnet-0f4ba4b32ec2d3452"] }  
 variable "public_subnet_ids" { default = ["subnet-02983905d30bafdc4", "subnet-0276f7262ebf25799"] }
 
 variable "my_public_ip" {
