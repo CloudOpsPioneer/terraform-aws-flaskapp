7e69c8ff0e5e204e79b3d39d96c48cf8dbfdedd9
karthikrajkkr <34535249+karthikrajkkr@users.noreply.github.com>
2024-10-11 12:09:35 -0400

    Update kube_deployment.tf

--- app-eks/terraform/kube_deployment.tf
+++ app-eks/terraform/kube_deployment.tf
@@ -57,3 +57,43 @@ resource "kubernetes_deployment_v1" "flask_deployment" {
   }
   depends_on = [aws_eks_node_group.webapp_node_1]
 }
+
+
+/*
+# YAML format of Deployment for better understanding
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: flask-deployment
+  labels:
+    app: flask
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: flask
+  template:
+    metadata:
+      labels:
+        app: flask
+    spec:
+      containers:
+        - name: flask-ui
+          image: <YOUR_ECR_REPOSITORY_URI>  
+          ports:
+            - containerPort: 8001
+          resources:
+            limits:
+              cpu: "0.5"
+              memory: "512Mi"
+            requests:
+              cpu: "250m"
+              memory: "50Mi"
+          # Uncomment the following if you wish to enable liveness probe
+          # livenessProbe:
+          #   httpGet:
+          #     path: "/"
+          #     port: 80
+          #   initialDelaySeconds: 3
+          #   periodSeconds: 3
+*/
